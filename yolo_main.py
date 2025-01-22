a74100321f1751be5e1736a9ba49c7c7a16064e7
Barbarik <kukun2003@mail.ru>
2025-01-22 20:58:55 +0300

    ready nn

--- yolo_main.py
+++ yolo_main.py
@@ -1,5 +1,12 @@
 from ultralytics import YOLO
 from roboflow import Roboflow
+import torch 
+
+
+model = YOLO("weights/best.pt")
+
+results = model("c:/Users/kukun/Downloads/HfapG-ZhjC8.jpg")
+results[0].show()
 
 """
 #установка дата сета
@@ -7,7 +14,7 @@ rf = Roboflow(api_key="7lPCpGpEFFZwytwZG6vt")
 project = rf.workspace("vgtu-n8zmy").project("military-vehicle")
 version = project.version(4)
 dataset = version.download("yolov11")
-"""
+
 model = YOLO("yolo11m.pt")
 
 model.train(data="military-vehicle-4/data.yaml", imgsz = 640, batch = 16, epochs = 10, device = "cpu")
@@ -16,8 +23,9 @@ model.train(data="military-vehicle-4/data.yaml", imgsz = 640, batch = 16, epochs
 metrics = model.val()
 
 # Perform object detection on an image
-results = model("C:/Users/kukun/Desktop/диплом/military-vehicle-4/test/images/5_jpg.rf.2cb0f27a60274f89284f474f01932dc6.jpg")
+
 results[0].show()
 
 # Export the model to ONNX format
 path = model.export(format="onnx")  # return path to exported model
+"""
